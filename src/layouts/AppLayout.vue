1fb57e887026f1952c1f6e12f4f5eab8f182e2f7
sofianelamine772-design <mwcrea.agency@gmail.com>
2026-05-04 18:14:36 +0200

    chore: rename Planning Stagiaires to Planning in sidebar

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -47,7 +47,7 @@ const navItems = [
   { name: 'Encaissement', icon: ShoppingCart, path: '/pos' },
   { name: 'Projets', icon: Briefcase, path: '/projects' },
   { name: 'Suivi Logiciels', icon: Monitor, path: '/software' },
-  { name: 'Planning Stagiaires', icon: Calendar, path: '/calendar' },
+  { name: 'Planning', icon: Calendar, path: '/calendar' },
   { name: 'Suivi Poseurs', icon: Truck, path: '/installers' },
   { name: 'Clients', icon: Users, path: '/customers' },
   { name: 'Suivis Clients', icon: MessageSquareText, path: '/tracking' },
